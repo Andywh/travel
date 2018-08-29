b059c1dbba7b4326825b73089b65a3a5741c7034
Andy <isongliang@qq.com>
2018-08-29 21:30:37 +0800

    online

---
+++ src/pages/detail/components/Banner.vue
@@ -0,0 +1,60 @@
+<template>
+  <div>
+    <div class="banner">
+      <img class="banner-img" src="http://img1.qunarzz.com/sight/p0/1506/30/7a528fae12c17e6e29d5d13a9d9813ee.water.jpg_600x330_866232e3.jpg" />
+      <div class="banner-info">
+        <div class="banner-title">
+          上海玛雅海滩水公园
+        </div>
+        <div class="banner-number">
+          <span class="iconfont banner-icon">&#xe692;</span>
+          39
+        </div>
+      </div>
+    </div>
+    <common-gallary></common-gallary>
+  </div>
+</template>
+
+<script>
+import CommonGallary from 'common/gallary/Gallary'
+export default {
+  name: 'DetailBanner',
+  components: {
+    CommonGallary
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+.banner
+  position: relative
+  overflow: hidden
+  height: 0
+  padding-bottom: 55%
+  .banner-img
+    width: 100%
+  .banner-info
+    display: flex
+    position: absolute
+    left: 0
+    right: 0
+    bottom: 0
+    line-height: .6rem
+    color: #fff
+    background-image: linear-gradient(top, rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.8))
+    .banner-title
+      flex: 1
+      font-size: .32rem
+      padding: 0 .2rem
+    .banner-number
+      height: .32rem
+      line-height: .32rem
+      margein-top: .14rem
+      padding: 0 .4rem
+      border-radius: .2rem
+      background: rgba(0, 0, 0, .8)
+      font-size: .24rem
+      .banner-icon
+        font-size: .24rem
+</style>
--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -57,7 +57,7 @@ export default {
     this.lastCity = this.city
     this.getHomeInfo()
   },
-  activated() {
+  activated () {
     if (this.lastCity !== this.city) {
       this.lastCity = this.city
       this.getHomeInfo()
